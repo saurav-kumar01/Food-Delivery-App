11b02210abb0b29cca6864d9d6082c16c3cc3077
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-08-23 14:17:23 +0530

    new update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,5 +3,5 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(`${process.env.MONGOBD_URI}`)
-    .then(() => console.log("DB connected"));
+    .then(() => console.log("DB connected successfull"));
 };
