d1cc98811d3b41d1a85ad0dc4fd340a2083230b5
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-08-23 14:13:45 +0530

    new update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-
 export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://greatstack:54752173@cluster0.vv1t9tm.mongodb.net/Food-Services').then(() =>console.log("DB connected"));
-}
+  await mongoose
+    .connect(`${process.env.MONGOBD_URI}`)
+    .then(() => console.log("DB connected"));
+};
