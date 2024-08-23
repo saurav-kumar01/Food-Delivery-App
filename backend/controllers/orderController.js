b472982c505f71ea2813e7bd5987badc8263a14a
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-08-23 16:32:40 +0530

    add url frontend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for payment
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5174";
+  const frontend_url = "https://food-delivery-frontend-xl3x.onrender.com";
 
   try {
     const newOrder = new orderModel({
