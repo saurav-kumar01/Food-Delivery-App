411bf40d76392e0499dd3bd05845b71f309f24cf
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-08-24 17:36:47 +0530

    completed MERN project

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,91 +1,87 @@
 import userModel from "../models/userModel.js";
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
-import validator from 'validator'
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import validator from "validator";
 
 //login user
 const loginUser = async (req, res) => {
-    const {email, password} = req.body;
+  const { email, password } = req.body;
 
-    try {
-        //checking user registered or not
-        const user = await userModel.findOne({email});
+  try {
+    //checking user registered or not
+    const user = await userModel.findOne({ email });
 
-        if(!user){
-            return res.json({success: false, message: "user doesn't exist"});
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if(!isMatch){
-            return res.json({success: false, message: "password is invalid"});
-        }
-
-        //send token to the user
-        const token = createToken(user._id);
-
-        res.json({success: true, token})
-    } catch (error) {
-        console.log(error);
-        res.json({success: false, message: "error: while login the user"});
-        
+    if (!user) {
+      return res.json({ success: false, message: "user doesn't exist" });
     }
-}
 
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "password is invalid" });
+    }
+
+    //send token to the user
+    const token = createToken(user._id);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "error: while login the user" });
+  }
+};
 
 //create token
 const createToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET);
-}
-
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
 //register user
 const registerUser = async (req, res) => {
-    
-    const {name, email, password} = req.body;
+  const { name, email, password } = req.body;
 
-    try {
-        //checking user is already exists or not
-        const exists = await userModel.findOne({email});
-        if(exists) {
-            return res.json({success: false, message: "user already exists"});
-
-        }
-
-        //validating email format and strong password
-        if(!validator.isEmail(email)){
-            return res.json({success: false, message: "Please enter a valid email"});
-        }
-
-        if(password.length < 8){
-            return res.json({success: false, message: "Please enter a strong password with the length more the 8"});
-        }
-
-        //hashing user password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        const newUser = new userModel({
-            name: name,
-            email: email,
-            password: hashedPassword
-        })
-
-        //save the user details in the database
-        const user = await newUser.save();
-
-        //token setup
-        const token = createToken(user._id);
-        res.json({success: true, token});
-
-    } catch (error) {
-        console.log(error);
-        res.json({success: false, message: "error: while register the user"})
-        
+  try {
+    //checking user is already exists or not
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "user already exists" });
     }
-}
 
+    //validating email format and strong password
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
 
-export {loginUser, registerUser}
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password with the length more the 8",
+      });
+    }
 
+    //hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+
+    //save the user details in the database
+    const user = await newUser.save();
+
+    //token setup
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "error: while register the user" });
+  }
+};
+
+export { loginUser, registerUser };
