2f29438b0e638c340173c372f115d8066118675e
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-08-24 16:58:00 +0530

    update

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,12 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './PlaceOrder.css'
-import { StoreContext } from '../../context/StoreContext'
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function PlaceOrder() {
-
-  const {getTotalCardAmount, token, food_list, cartItems, url} = useContext(StoreContext);
+  const { getTotalCardAmount, token, food_list, cartItems, url } =
+    useContext(StoreContext);
 
   const [data, setData] = useState({
     firstName: "",
@@ -17,19 +17,19 @@ function PlaceOrder() {
     state: "",
     zipcode: "",
     country: "",
-    phone: ""
-  })
+    phone: "",
+  });
 
   const onChangeHandler = (e) => {
     const name = e.target.name;
     const value = e.target.value;
 
-    setData(data=>({...data, [name]:value}));
-  }
+    setData((data) => ({ ...data, [name]: value }));
+  };
 
   // useEffect(()=> {
   //   console.log(data);
-    
+
   // }, [data])
 
   const placeOrder = async (e) => {
@@ -42,87 +42,150 @@ function PlaceOrder() {
         itemInfo["quantity"] = cartItems[item._id];
         orderItems.push(itemInfo);
       }
-    })
+    });
     // console.log(orderItems);
     let orderData = {
       address: data,
       items: orderItems,
-      amount: getTotalCardAmount()+2,
-    }
-    
+      amount: getTotalCardAmount() + 2,
+    };
+
     // console.log(url);
-    
+
     //send orderData with axios
-    let response = await axios.post(url+"/api/order/place", orderData, {headers: {token}});
+    let response = await axios.post(url + "/api/order/place", orderData, {
+      headers: { token },
+    });
     console.log(response);
-    
+
     if (response.data.success) {
-      const {session_url} = response.data;
+      const { session_url } = response.data;
       window.location.replace(session_url);
     } else {
-      alert("error: while sending session")
+      alert("error: while sending session");
     }
-  }
-
+  };
 
   const navigate = useNavigate();
 
-
-  useEffect(()=> {
+  useEffect(() => {
     if (!token) {
-      navigate('/cart')
+      navigate("/cart");
+    } else if (getTotalCardAmount() === 0) {
+      navigate("/cart");
     }
-    else if(getTotalCardAmount() === 0){
-      navigate('/cart')
-    }
-  }, [token])
-
+  }, [token]);
 
   return (
-    <form onSubmit={placeOrder} className='place-order'>
+    <form onSubmit={placeOrder} className="place-order">
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input required name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='First name'/>
-          <input required name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder='Last name'/>
+          <input
+            required
+            name="firstName"
+            onChange={onChangeHandler}
+            value={data.firstName}
+            type="text"
+            placeholder="First name"
+          />
+          <input
+            required
+            name="lastName"
+            onChange={onChangeHandler}
+            value={data.lastName}
+            type="text"
+            placeholder="Last name"
+          />
         </div>
-        <input required name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Email Address'/>
-        <input required name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder='Street'/>
+        <input
+          required
+          name="email"
+          onChange={onChangeHandler}
+          value={data.email}
+          type="email"
+          placeholder="Email Address"
+        />
+        <input
+          required
+          name="street"
+          onChange={onChangeHandler}
+          value={data.street}
+          type="text"
+          placeholder="Street"
+        />
         <div className="multi-fields">
-          <input required name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City'/>
-          <input required name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='State'/>
+          <input
+            required
+            name="city"
+            onChange={onChangeHandler}
+            value={data.city}
+            type="text"
+            placeholder="City"
+          />
+          <input
+            required
+            name="state"
+            onChange={onChangeHandler}
+            value={data.state}
+            type="text"
+            placeholder="State"
+          />
         </div>
         <div className="multi-fields">
-          <input required name='zipcode' onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='Zip code'/>
-          <input required name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country'/>
+          <input
+            required
+            name="zipcode"
+            onChange={onChangeHandler}
+            value={data.zipcode}
+            type="text"
+            placeholder="Zip code"
+          />
+          <input
+            required
+            name="country"
+            onChange={onChangeHandler}
+            value={data.country}
+            type="text"
+            placeholder="Country"
+          />
         </div>
-        <input required name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder='Phone'/>
+        <input
+          required
+          name="phone"
+          onChange={onChangeHandler}
+          value={data.phone}
+          type="text"
+          placeholder="Phone"
+        />
       </div>
 
       <div className="place-order-right">
         <div className="cart-total">
-            <h2>Cart Total</h2>
-            <div>
-              <div className="cart-total-details">
-                <p>Subtotal</p>
-                <p>${getTotalCardAmount()}</p>
-              </div>
-              <hr />
-              <div className="cart-total-details">
-                <p>Delivery fee</p>
-                <p>${getTotalCardAmount() === 0 ? 0 : 2}</p>
-              </div>
-              <hr />
-              <div className="cart-total-details">
-                <b>Total</b>
-                <b>${getTotalCardAmount() === 0 ? 0 :getTotalCardAmount() + 2}</b>
-              </div>
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCardAmount()}</p>
             </div>
-            <button type='submit'>PROCEED TO PAYMENT</button>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery fee</p>
+              <p>${getTotalCardAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>
+                ${getTotalCardAmount() === 0 ? 0 : getTotalCardAmount() + 2}
+              </b>
+            </div>
+          </div>
+          <button type="submit">PROCEED TO PAYMENT</button>
         </div>
       </div>
     </form>
-  )
+  );
 }
 
-export default PlaceOrder
+export default PlaceOrder;
