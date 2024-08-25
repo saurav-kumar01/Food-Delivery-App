a141b1aa88cc58380dac67104d453d81b143fb21
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-08-26 02:01:21 +0530

    Change URL for Backend

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "https://food-delivery-admin-yg4b.onrender.com";
+  const url = "https://food-delivery-backend-6v2q.onrender.com";
 
   return (
     <div>
--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://food-delivery-admin-yg4b.onrender.com";
+  const url = "https://food-delivery-backend-6v2q.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
