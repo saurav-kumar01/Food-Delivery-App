b6957958821829c6b94befa2b89afdb2ad0e5db6
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-08-25 23:53:19 +0530

    new update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://sauravKumar:54752173@food-del.ptykp.mongodb.net/Food-Services"
+      "mongodb+srv://sauravKumar:80773201@food-del.ptykp.mongodb.net/Food-Services"
     )
     .then(() => console.log("Database connected"));
 };
