ef859889fe7e42a5a72e67e0162e77b7cfda1424
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-08-26 00:22:02 +0530

    completed MERN project

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
-// import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
