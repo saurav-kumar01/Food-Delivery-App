6a8689f5da6696dc4e7f691a705384b90020b0e3
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-08-26 00:20:07 +0530

    cpmleted MERN project

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -67,7 +67,7 @@ const StoreContextProvider = (props) => {
       await fetchFoodList();
       if (localStorage.getItem("token")) {
         setToken(localStorage.getItem("token"));
-        // await loadCartData(localStorage.getItem("token"));
+        await loadCartData(localStorage.getItem("token"));
       }
     }
     loadData();
